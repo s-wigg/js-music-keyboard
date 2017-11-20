5ee392f29c66c43c43358de792b187456fe9a073
Severin Wiggenhorn <severin.randall@gmail.com>
2017-11-20 14:16:53 -0800

    added keyboard functionality

--- noise.js
+++ noise.js
@@ -1,8 +1,16 @@
 $(document).ready(() => {
   $('.instrument').on('click', 'button', function(event) {
-    let audioElement = document.getElementById(`${$(this).html()}Audio`);
-    // console.log(audioElement);
+    const audioElement = document.getElementById(`${$(this).html()}Audio`);
     audioElement.load();
     audioElement.play();
+  });
+
+  $('body').keydown ((event)  => {
+    const musicalNotes = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    if (musicalNotes.includes(event.key)) {
+    let audioElement = document.getElementById(`${event.key}Audio`);
+    audioElement.load();
+    audioElement.play();
+    }
   });
 });
